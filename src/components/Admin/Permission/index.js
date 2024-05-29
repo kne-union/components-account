e348b788f6b928445b31d0f00cf5cad41b42007c
zhipenglin <sunandmoon001@163.com>
2024-05-29 18:49:31 +0800

    添加应用权限设置

--- src/components/Admin/Permission/index.js
+++ src/components/Admin/Permission/index.js
@@ -1,16 +1,47 @@
 import { createWithRemoteLoader } from '@kne/remote-loader';
-import { Button, Menu } from 'antd';
+import { Button, Menu, App } from 'antd';
 import mockList from './mock/permission-list.json';
 import { useMemo, useState } from 'react';
 import style from './style.module.scss';
 import Detail from './Detail';
+import ApplicationFormInner from './ApplicationFormInner';
+import Fetch from '@kne/react-fetch';
+
+const ApplicationList = createWithRemoteLoader({
+  modules: ['components-core:Global@usePreset']
+})(({ remoteModules, current, onChange }) => {
+  const [usePreset] = remoteModules;
+  const { apis } = usePreset();
+  return (
+    <Fetch
+      {...Object.assign({}, apis.account.getApplicationList)}
+      render={({ data }) => {
+        return (
+          <Menu
+            items={data.map(item => {
+              return {
+                label: `${item.name}(${item.code})`,
+                key: item.code
+              };
+            })}
+            className={style['menu-outer']}
+            selectedKeys={current}
+            onClick={({ key }) => onChange(key)}
+          />
+        );
+      }}
+    />
+  );
+});
 
 const Permission = createWithRemoteLoader({
-  modules: ['components-core:Layout@Page']
+  modules: ['components-core:Layout@Page', 'components-core:FormInfo@useFormModel', 'components-core:Global@usePreset']
 })(({ remoteModules }) => {
-  const [Page] = remoteModules;
+  const [Page, useFormModel, usePreset] = remoteModules;
   const [current, setCurrent] = useState('');
-
+  const { ajax, apis } = usePreset();
+  const { message } = App.useApp();
+  const formModel = useFormModel();
   const mockListData = useMemo(() => {
     const list = (mockList.data || []).map(({ id, name }) => ({ key: id.toString(), label: name }));
     if (list.length) {
@@ -20,9 +51,37 @@ const Permission = createWithRemoteLoader({
   }, [mockList.data]);
 
   return (
-    <Page title="应用权限管理" titleExtra={<Button type="primary">添加</Button>}>
+    <Page
+      title="应用权限管理"
+      titleExtra={
+        <Button
+          type="primary"
+          onClick={() => {
+            formModel({
+              title: '添加应用',
+              formProps: {
+                onSubmit: async data => {
+                  const { data: resData } = await ajax(
+                    Object.assign({}, apis.account.addApplication, {
+                      data
+                    })
+                  );
+                  if (resData.code !== 0) {
+                    return;
+                  }
+                  message.success('添加应用成功');
+                }
+              },
+              children: <ApplicationFormInner />
+            });
+          }}
+        >
+          添加应用
+        </Button>
+      }
+    >
       <div className={style['flex-wrapper']}>
-        <Menu items={mockListData} className={style['menu-outer']} selectedKeys={current} onClick={({ key }) => setCurrent(key)} />
+        <ApplicationList current={current} onChange={setCurrent} />
         <Detail
           params={{ id: current }}
           id={current}
