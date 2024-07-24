1b33b70a9cf0a2d08abda3a2f308f2e5a67f6900
jinfanghuang <2283785225@qq.com>
2024-07-24 14:14:32 +0800

    feat: 类型筛选

--- src/components/Admin/OperationLog/index.js
+++ src/components/Admin/OperationLog/index.js
@@ -1,17 +1,20 @@
 import { createWithRemoteLoader } from '@kne/remote-loader';
 import { getOperationLogListColumns, OperationLogListOptions } from '@components/Setting';
+import { useState } from 'react';
 
 const OperationLog = createWithRemoteLoader({
-  modules: ['components-core:Layout@TablePage', 'components-core:Global@usePreset', 'components-core:Filter@getFilterValue']
+  modules: ['components-core:Layout@TablePage', 'components-core:Global@usePreset', 'components-core:Filter@getFilterValue', 'components-core:StateBar']
 })(({ remoteModules, menu }) => {
-  const [TablePage, usePreset, getFilterValue] = remoteModules;
+  const [TablePage, usePreset, getFilterValue, StateBar] = remoteModules;
   const { apis } = usePreset();
+  const [currentSelectedKey, setCurrentSelectedKey] = useState('user');
+
   return (
     <OperationLogListOptions>
       {({ ref, filter }) => {
         return (
           <TablePage
-            {...Object.assign({}, apis.account.admin.getOperationLogList, { data: getFilterValue(filter.value) })}
+            {...Object.assign({}, apis.account.admin.getOperationLogList, { data: { filter: Object.assign({}, getFilterValue(filter.value), { type: currentSelectedKey }) } })}
             ref={ref}
             name="log"
             columns={[...getOperationLogListColumns()]}
@@ -20,6 +23,19 @@ const OperationLog = createWithRemoteLoader({
               filter,
               menu
             }}
+            topArea={
+              <StateBar
+                type="radio"
+                size="small"
+                activeKey={currentSelectedKey}
+                stateOption={[
+                  { key: 'user', tab: '用户' },
+                  { key: 'tenant', tab: '租户' },
+                  { key: 'admin', tab: '管理员' }
+                ]}
+                onChange={setCurrentSelectedKey}
+              />
+            }
           >
             操作日志
           </TablePage>
